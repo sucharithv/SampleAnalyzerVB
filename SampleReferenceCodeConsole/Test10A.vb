0e5ffc4bd4b8e13bfe01b8caf6681c03a4e51003
Sucharith <sucharithv@gmail.com>
2020-09-03 23:27:29 -0400

    Added new unit tests  'Test 10A  If Client?.DateOfBirth.HasValue = True AndAlso Client?.DateOfBirth <> Date.MinValue Then 'Test12  ' Should not be an issue as HasValue is checked first  If budgetEligibilityBO.ProcessCode.HasValue AndAlso budgetEligibilityBO.ProcessCode <> ProcessCodes.NonContinuousEligibility Then

---
+++ SampleReferenceCodeConsole/Test10A.vb
@@ -0,0 +1,32 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class Test10A
+    Sub EvaluateHasValueWithNullConditionEqualsTrue()
+        Dim fooInstance As Foo = Nothing
+        WriteLog("fooInstance?.BeginDate.HasValue AndAlso fooInstance.BeginDate > System.DateTime.MinValue...")
+        If fooInstance?.BeginDate.HasValue = True AndAlso fooInstance.BeginDate > System.DateTime.MinValue Then
+            WriteLog("fooInstance?.BeginDate.HasValue AndAlso fooInstance.BeginDate > System.DateTime.MinValue evaluated to TRUE")
+        Else
+            WriteLog("fooInstance?.BeginDate.HasValue AndAlso fooInstance.BeginDate > System.DateTime.MinValue evaluated to FALSE")
+        End If
+    End Sub
+
+    Private Function AlwaysTrue() As Boolean
+        Return True
+    End Function
+
+    Private Function AlwaysFalse() As Boolean
+        Return False
+    End Function
+
+    Sub WriteLog(ByVal message As String)
+        System.Diagnostics.Debug.WriteLine(message)
+    End Sub
+
+    Private Class Foo
+
+        Public Property BeginDate() As System.Nullable(Of Date)
+
+    End Class
+End Class
---
+++ SampleReferenceCodeConsole/Test12.vb
@@ -0,0 +1,38 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class Test12
+    Sub EvaluatedHasValueFirst()
+        Dim budgetEligibilityBO As BudgetEligibilityBO = Nothing
+        WriteLog("budgetEligibilityBO.ProcessCode.HasValue AndAlso budgetEligibilityBO.ProcessCode <> ProcessCodes.NonContinuousEligibility...")
+        If budgetEligibilityBO.ProcessCode.HasValue AndAlso
+            budgetEligibilityBO.ProcessCode <> ProcessCodes.NonContinuousEligibility Then
+            WriteLog("evaluated to TRUE")
+        Else
+            WriteLog("evaluated to FALSE")
+        End If
+    End Sub
+
+    Private Function AlwaysTrue() As Boolean
+        Return True
+    End Function
+
+    Private Function AlwaysFalse() As Boolean
+        Return False
+    End Function
+
+    Sub WriteLog(ByVal message As String)
+        System.Diagnostics.Debug.WriteLine(message)
+    End Sub
+
+    Private Class BudgetEligibilityBO
+
+        Public Property ProcessCode As System.Nullable(Of ProcessCodes)
+
+    End Class
+
+    Protected Enum ProcessCodes
+        NonContinuousEligibility = 1
+        RecurringBenefit = 2
+    End Enum
+End Class
